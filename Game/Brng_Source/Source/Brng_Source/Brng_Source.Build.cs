845e24a5a8500b9639e94616ee2f20b75f5194eb
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2021-07-06 22:33:04 -0700

    Created main menu logic

--- Game/Brng_Source/Source/Brng_Source/Brng_Source.Build.cs
+++ Game/Brng_Source/Source/Brng_Source/Brng_Source.Build.cs
@@ -8,6 +8,6 @@ public class Brng_Source : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" , "UMG", "Slate", "SlateCore"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" , "UMG", "Slate", "SlateCore", "OnlineSubsystem", "OnlineSubsystemUtils"});
 	}
 }
